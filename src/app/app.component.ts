4774ff1c8a152a27929600a15590774b49d4a7ec
Leifer Mendez <leifer33@gmail.com>
2020-05-22 13:34:27 +0200

    Btn

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app-argentina';
+
+  public constructor(private router: Router) {
+    document.addEventListener('backbutton', () => {
+      console.log('Back BTN')
+      this.router.navigateByUrl('/');
+    });
+
+  }
+
 }
