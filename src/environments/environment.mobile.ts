28c19e076b3edaea1ca6797ce48a7f37019df290
Leifer Mendez <leifer33@gmail.com>
2020-06-04 21:23:37 +0200

    Cambios realizados

--- src/environments/environment.mobile.ts
+++ src/environments/environment.mobile.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   mobile: true,
-  api: 'https://c1371307.ferozo.com/API/public/api/1.0',
+  api: 'https://controlderesidencialesparaadultos.com.ar/API/public/api/1.0',
   daysTokenExpire: 4
 };
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   mobile: false,
-  api: 'https://c1371307.ferozo.com/API/public/api/1.0',
+  api: 'https://controlderesidencialesparaadultos.com.ar/API/public/api/1.0',
   daysTokenExpire: 4
 };
